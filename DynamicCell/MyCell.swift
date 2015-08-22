0888789008c0b2a9abd88f8fb50239bcc0b80edd
nemesis <john@kaliakatsos.net>
2015-08-22 02:03:34 +0200

    Initial commit

---
+++ DynamicCell/MyCell.swift
@@ -0,0 +1,46 @@
+//
+//  MyCell.swift
+//  DynamicTable
+//
+//  Created by Ioannis Kaliakatsos on 21/08/2015.
+//  Copyright (c) 2015 Ioannis Kaliakatsos. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+    
+    static let identifier = "cell identifier"
+    var photoImageView: UIImageView!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Default, reuseIdentifier: MyCell.identifier)
+        translatesAutoresizingMaskIntoConstraints = false
+        photoImageView = UIImageView(image: UIImage(named: "photo"))
+        photoImageView.contentMode = .ScaleAspectFit
+        contentView.addSubview(photoImageView)
+        setNeedsUpdateConstraints()
+    }
+    
+    override func updateConstraintsIfNeeded() {
+        let viewAttributes: [NSLayoutAttribute] = [.Top, .Trailing, .Leading, .Bottom]
+        for attribute in viewAttributes {
+            let constraint = NSLayoutConstraint(
+                item: photoImageView,
+                attribute: attribute,
+                relatedBy: .Equal,
+                toItem: self,
+                attribute: attribute,
+                multiplier: 1,
+                constant: 0)
+            addConstraint(constraint)
+        }
+        
+        super.updateConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ DynamicCell/MyController.swift
@@ -0,0 +1,31 @@
+//
+//  MyController.swift
+//  DynamicTable
+//
+//  Created by Ioannis Kaliakatsos on 21/08/2015.
+//  Copyright (c) 2015 Ioannis Kaliakatsos. All rights reserved.
+//
+
+import UIKit
+
+class MyController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(MyCell.self, forCellReuseIdentifier: MyCell.identifier)
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(MyCell.identifier, forIndexPath: indexPath) as! MyCell
+        cell.layoutIfNeeded()
+        return cell
+    }
+}
